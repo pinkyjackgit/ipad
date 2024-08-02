4bfd2b925374ef2b2029402fc047d1400a6718ae
pinky <pinkygithub@gmail.com>
2024-08-02 21:51:33 +0100

    feat: rock paper & scissors

--- index.js
+++ index.js
@@ -16,7 +16,46 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  return prompt("Enter: Rock, Paper or Scissors", "Rock");
+  let choice = prompt("Enter: Rock, Paper or Scissors", "Rock");
+  return choice.toLowerCase();
 }
 
 // round logic
+
+function playGame() {
+  let humanSelection = getHumanChoice();
+  let computerSelection = getComputerChoice();
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanSelection == computerChoice) {
+      console.log(`Draw: ${humanChoice} = ${computerChoice}`);
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+      console.log(`Computer wins: ${computerChoice} beats ${humanChoice}`);
+      return ++computerScore;
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+      console.log(`Computer wins: ${computerChoice} beats ${humanChoice}`);
+      return ++computerScore;
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+      console.log(`Computer wins: ${computerChoice} beats ${humanChoice}`);
+      return ++computerScore;
+    } else {
+      console.log(`You win: ${humanChoice} beats ${computerChoice}`);
+      return ++humanScore;
+    }
+  }
+  playRound(humanSelection, computerSelection);
+}
+
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
+
+if (humanScore > computerScore) {
+  console.log(`You win ${humanScore} : ${computerScore}`);
+} else if (humanScore < computerScore) {
+  console.log(`You lose ${computerScore} : ${humanScore}`);
+} else {
+  console.log(`Draw ${humanScore} : ${computerScore}`);
+}
